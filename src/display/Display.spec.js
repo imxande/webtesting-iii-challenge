b7ddd8c6542337e06e4e4ef32bf9cbb53659e3d7
imxande <alerodriguezlarosa@gmail.com>
2019-12-16 17:42:49 -0500

    test for closed or locked gate

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,22 @@
 // Test away!
+import React from 'react';
+import Display from './Display';
+import {render} from '@testing-library/react';
+
+
+test('check if closed', () =>{
+    const {getByTestId, rerender} = render(<Display closed/>)
+    getByTestId('closed');
+
+    rerender(<Display closed = {false} />)
+    getByTestId('closed')
+})
+
+test('check if locked', () =>{
+    const {getByTestId, rerender} = render(<Display locked/>)
+    getByTestId('locked');
+
+    rerender(<Display locked = {false} />)
+    getByTestId('locked')
+})
+
