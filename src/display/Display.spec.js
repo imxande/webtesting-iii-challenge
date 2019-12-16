34ef69af7cfc10271f25a3b7f952b450b6a04648
imxande <alerodriguezlarosa@gmail.com>
2019-12-16 18:05:41 -0500

    display test done

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -20,3 +20,28 @@ test('check if locked', () =>{
     getByTestId('locked')
 })
 
+test('check if green red', () => {
+    const className = 'led red-led';
+    const locked = false;
+    const display = render(<Display closed locked = {locked}
+    className = {className}/>)
+    expect(display.baseElement).toMatchSnapshot();
+})
+
+test('check if green green',() => {
+    const className = 'led green-led';
+    const locked = false;
+    const closed = false;
+    const display = render(<Display closed locked = {locked}
+    className = {className}/>)
+    expect(display.baseElement).toMatchSnapshot();
+})
+
+test('check if red red',() => {
+    const className = 'led red-led';
+    const locked = true;
+    const closed = true;
+    const display = render(<Display closed = {closed} locked = {locked}
+    className = {className}/>)
+    expect(display.baseElement).toMatchSnapshot();
+})
