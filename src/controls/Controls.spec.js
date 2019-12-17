1a66fdea7ef46763bfae7dd605c6ac233650033c
imxande <alerodriguezlarosa@gmail.com>
2019-12-17 10:26:14 -0500

    fixed dashboard testing and controls

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import Controls from './Controls';
 import {render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 
 
 test('close gate button change to open', () => {
@@ -18,3 +19,34 @@ test('close gate button change to open', () => {
   expect(button.innerHTML).toBe('Open Gate')
  
 });
+
+test('lock gate button text changes to unlock gate', () =>{
+  const constProps = {closed: true, locked: false}
+  const jestFn = jest.fn(() =>{
+    constProps.locked = !constProps.locked;
+  })
+  const {getByText, rerender} = render(<Controls {...constProps} toggleLocked = {jestFn} />);
+  const button = getByText(/lock gate/i);
+  fireEvent.click(button);
+  rerender(<Controls {...constProps} />)
+  expect(button.innerHTML).toBe('Unlock Gate')
+ 
+})
+
+test('check if lock/unlock is disabled when gate is open', ()=>{
+  const constProps = {closed: false, locked: false}
+  const {getByText} = render(<Controls {...constProps}/>)
+ expect(getByText(/lock gate/i).closest('button')).toHaveAttribute('disabled')
+})
+
+test('check if Open/closed is disbled when gate is lock', () =>{
+  const constProps = {closed: true, locked: true}
+  const {getByText} = render(<Controls {...constProps}/>)
+ expect(getByText(/open gate/i).closest('button')).toHaveAttribute('disabled')
+})
+
+test('provide button from closed and open', () =>{
+  const constProps = {closed: false, locked: false}
+  const {getByText} = render(<Controls {...constProps}/>)
+  getByText(/close gate/i)
+})
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,14 +1,14 @@
-// // Test away
-// import React from 'react';
-// import Dashboard from './Dashboard';
-// import {render} from '@testing-library/react';
+// Test away
+import React from 'react';
+import Dashboard from './Dashboard';
+import {render} from '@testing-library/react';
 
 
-// test('shows the controls and display', () =>{
-//     const {getByText, rerender} = render(<Dashboard/>)
-//     getAllByText(/lock/i)
+test('shows the controls and display', () =>{
+    const {getAllByText, rerender} = render(<Dashboard/>)
+    getAllByText(/lock/i)
 
-//     rerender(<Dashboard />)
-//     getAllByText(/lock/i)
-// })
+    // rerender(<Dashboard />)
+    // getAllByText(/lock/i)
+})
 
