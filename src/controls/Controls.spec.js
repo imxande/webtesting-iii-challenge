0c1d327b79458df0f4ecb37959a06bf82c9a93f3
imxande <alerodriguezlarosa@gmail.com>
2019-12-16 19:22:20 -0500

    Control test

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,20 @@
 // Test away!
+import React from 'react';
+import Controls from './Controls';
+import {render, fireEvent } from '@testing-library/react';
+
+
+test('close gate button change to open', () => {
+    const constProps = {closed: false, locked: false}
+    const myVar =  jest.fn(() => {
+        constProps.closed = !constProps.closed;
+      })
+     
+ 
+  const { getByText, rerender } = render(<Controls {...constProps} toggleClosed = {myVar} />);
+  const button = getByText(/close gate/i);
+  fireEvent.click(button);
+  rerender(<Controls {...constProps} toggleClosed = {myVar}/>)
+  expect(button.innerHTML).toBe('Open Gate')
+ 
+});
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,14 @@
-// Test away
+// // Test away
+// import React from 'react';
+// import Dashboard from './Dashboard';
+// import {render} from '@testing-library/react';
+
+
+// test('shows the controls and display', () =>{
+//     const {getByText, rerender} = render(<Dashboard/>)
+//     getAllByText(/lock/i)
+
+//     rerender(<Dashboard />)
+//     getAllByText(/lock/i)
+// })
+
